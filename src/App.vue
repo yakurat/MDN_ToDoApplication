48b88cfbbbe2cc772eaa2c4c6cb5b485fa72098d
ISTYLE\yakurat <yakurat@istyle.co.jp>
2021-10-01 16:59:12 +0900

    feat post and css

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <h1>To-DO List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <!-- v-for Vueの機能 配列に要素をマッピングする-->
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+        ></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import ToDoItem from "./components/ToDoitem.vue";
+import ToDoForm from "./components/ToDoform.vue";
+import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    "to-do-item": ToDoItem,
+    "to-do-form": ToDoForm,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Created todo list", done: false },
+      ],
+    };
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      console.log("To-do added", toDoLabel);
+      this.ToDoItems.push({
+        id: uniqueId("todo-"),
+        label: toDoLabel,
+        done: false,
+      });
+    },
   },
 };
 </script>
---
+++ src/components/ToDoitem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="checkbox" v-bind:id="id" v-bind:checked="isDone" />
+    <label v-bind:for="id">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+    id: { required: true, type: String },
+  },
+  data() {
+    return {
+      isDone: this.done,
+      // lodash.uniqueid 一意のidを割り振ってくれる外部モジュール下の例だと todo-xxxxみたいな割り振り方
+    };
+  },
+};
+</script>
